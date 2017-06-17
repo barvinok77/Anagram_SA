710bf211dc7bd10d2039d115644ba74ae4b73514
t_aleksandr <b.arvinok1977@gmail.com>
2017-06-17 14:22:16 -0700

    Fixed SA violations

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -2,8 +2,8 @@ package core;
 
 public class App {
     
-	public static void main( String[] args ) {
-        System.out.println( "Debit Card and Bad credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
-        System.out.println( "Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
+	public static void main(String[] args) {
+        System.out.println("Debit Card and Bad credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+        System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
     }
 }
